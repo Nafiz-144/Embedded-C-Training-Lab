100bdc69336b128714fa6c1722e469855c93ed76
Nafiz144 <nafizahamed0464@gmail.com>
2025-10-15 12:19:58 +0600

    10-25

--- Step-1-Master C Fundamentals/tempCodeRunnerFile.c
+++ Step-1-Master C Fundamentals/tempCodeRunnerFile.c
@@ -1,10 +1,12 @@
-#include<stdio.h>
-int main(){
-int a=5;
-int *p;
-p=&a;
-printf("Value of a is %d\n",a);
-printf("Address of a is %p\n",(void *)&a);
+#include <stdio.h>
+int main()
+{
+    int c;
+    int a = 5;
+    int *p;
+    p = &a;
+    printf("Value of a is %d\n", a);
+    printf("Address of a is %p\n", (void *)&a);
 
-printf("Address of a using pointer p is %p\n",(void *)p);
+    printf("Address of a using pointer p is %p\n", (void *)p);
 }
